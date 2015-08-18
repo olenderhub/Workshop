2ef00922fe63419bc26d2e79a110ebb4ff2b4baa
Hubert Olender <olender.hub@gmail.com>
2015-08-18 19:18:56 +0200

    Student decorator - fixed

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,13 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    if subject_item.subject_item_notes.present?
+      subjects = subject_item.subject_item_notes
+      sum_of_notes = subjects.inject(0) { |sum, note| sum + note.value.to_f }
+      number_of_notes = subjects.size
+      avg_notes = (sum_of_notes/number_of_notes).to_s + "0"
+    else
+      "0.00"
+    end
   end
 end
