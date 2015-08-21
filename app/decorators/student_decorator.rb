fb1ae1d3d8f13d893d4b7c85b3c5a9a949eddd40
Hubert Olender <olender.hub@gmail.com>
2015-08-21 19:36:38 +0200

    Fix some controllers

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -8,7 +8,7 @@ class StudentDecorator < BaseDecorator
       subjects = subject_item.subject_item_notes
       sum_of_notes = subjects.inject(0) { |sum, note| sum + note.value.to_f }
       number_of_notes = subjects.size
-      avg_notes = (sum_of_notes/number_of_notes).to_s + "0"
+      avg_notes = (sum_of_notes/number_of_notes).round(1).to_s + "0"
     else
       "0.00"
     end
